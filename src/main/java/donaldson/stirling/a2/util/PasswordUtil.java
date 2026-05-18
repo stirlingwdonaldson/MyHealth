2f6aaff00e5ee76c80b0b80ba90f070dec06eb2f
Stirling Donaldson <s4169686@student.rmit.edu.au>
2026-05-18 19:55:50 +1000

    feat(pwd-util): added simplistic hasher

--- src/main/java/donaldson/stirling/a2/util/PasswordUtil.java
+++ src/main/java/donaldson/stirling/a2/util/PasswordUtil.java
@@ -1,13 +1,29 @@
 package donaldson.stirling.a2.util;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public final class PasswordUtil {
 
   private PasswordUtil() {
 
   }
 
-  public static String hash(String rawPassword) {
-    return "hashed password";
+  public static String hash(String password) {
+
+    try{
+      MessageDigest digest = MessageDigest.getInstance("SHA-256");
+      byte[] hashBytes = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+
+      StringBuilder builder = new StringBuilder();
+      for(byte hashByte : hashBytes){
+        builder.append(String.format("%02x", hashByte));
+      }
+      return builder.toString();
+    } catch (NoSuchAlgorithmException exception){
+      throw new IllegalStateException("SHA-256 not available", exception);
+    }
   }
 
 }
