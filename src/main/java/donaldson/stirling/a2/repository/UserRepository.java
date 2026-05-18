c78cbcb140d4205df3b9da4c49f9eac59ee22691
Stirling Donaldson <s4169686@student.rmit.edu.au>
2026-05-18 19:36:20 +1000

    feat(user-repo): added username exists function

--- src/main/java/donaldson/stirling/a2/repository/UserRepository.java
+++ src/main/java/donaldson/stirling/a2/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package donaldson.stirling.a2.repository;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import donaldson.stirling.a2.model.User;
@@ -13,7 +15,15 @@ public class UserRepository {
   }
 
   public boolean usernameExists(String username) throws SQLException {
+    String sql = "SELECT 1 FROM users WHERE username = ?";
 
+    try (PreparedStatement statement = connection.prepareStatement(sql)) {
+      statement.setString(1, username);
+
+      try(ResultSet resultSet = statement.executeQuery()){
+        return resultSet.next();
+      }
+    }
   }
 
   public User createUser(String username, String hashedPassword, String firstName, String lastName)
